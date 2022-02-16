36eeb60a79b4c9b703b11fa51538eb0909269428
hectorchu~ <hector.chu828@gmail.com>
2022-02-16 12:32:23 -0800

    Bug: error is not being returned with query string containing non-integer

--- app.js
+++ app.js
@@ -7,20 +7,28 @@ const { findMode, findMean, findMedian } = require("./stats");
 const { convertStrNums } = require("./utils");
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
 app.get("/mean", function (req, res) {
-  let resp = req.query.nums; //should return str with q val
-  let nums = convertStrNums(resp);
-  return res.json({response: {
-    operation: "mean",
-    value: findMean(nums),
+    let resp = req.query.nums; //should return str with q val
+
+    if (resp.length === 0) {
+        throw new BadRequestError("Numbers are required.");
     }
-  });
+    let nums = convertStrNums(resp);
+
+    let mean = findMean(nums);
+
+    return res.json({
+        response: {
+            operation: "mean",
+            value: mean,
+        }
+    });
 })
 
 
@@ -32,15 +40,15 @@ app.get("/mean", function (req, res) {
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
-  throw new NotFoundError();
+    throw new NotFoundError();
 });
 
 /** Error handler: logs stacktrace and returns JSON error message. */
 app.use(function (err, req, res, next) {
-  const status = err.status || 500;
-  const message = err.message;
-  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
-  return res.status(status).json({ error: { message, status } });
+    const status = err.status || 500;
+    const message = err.message;
+    if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+    return res.status(status).json({ error: { message, status } });
 });
 
 
--- utils.js
+++ utils.js
@@ -4,11 +4,16 @@ const { BadRequestError } = require("./expressError");
 /** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
 
 function convertStrNums(strNums) {
-  // if the conversion isn't successful, throw a BadRequestError and will
-  // be handled in your route
-  let nums = strNums.split(",");
+    // if the conversion isn't successful, throw a BadRequestError and will
+    // be handled in your route
+    let nums = strNums.split(",");
 
-  return nums.map((num) => +num);
+    const convertedNums = nums.map((num) => +num);
+    if (convertedNums.includes(NaN)) {
+        throw new BadRequestError(`${num} is not a number.`);
+    }
+
+    return convertedNums;
 }
 
 
